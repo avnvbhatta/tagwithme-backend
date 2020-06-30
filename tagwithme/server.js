8004cf92c11f745298778ab6f2f13c71d328d0e6
abhinav <avnvbhatta@gmail.com>
2020-06-30 19:48:55 -0400

    working auth

--- tagwithme/server.js
+++ tagwithme/server.js
@@ -7,30 +7,33 @@ const port = 4000
 const db = require('./queries')
 const session = require('express-session')
 const passport = require('passport')
-
+const cookieParser = require("cookie-parser");
 const initializePassport = require('./passportConfig')
 initializePassport(passport);
 
 
 app.use(cors())
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+}));
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 )
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false,
-}));
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API.' })
 })
-app.get('/users', db.getUsers)
+
+app.get('/users', auth, db.getUsers)
 // app.get('/users/:id', db.getUserById)
 // app.post('/users', db.createUser)
 // app.put('/users/:id', db.updateUser)
@@ -40,27 +43,45 @@ app.get('/users', db.getUsers)
 
 app.post('/users/register', db.registerUser);
 
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/success',
-  failureRedirect: '/fail'
-}));
+
+
+// Routes
+app.post("/users/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        let userInfo = {
+          id: req.user.id,
+          name: req.user.name,
+          email: req.user.email
+        }
+        res.send(userInfo);
+      });
+    }
+  })(req, res, next);
+});
+
 
 app.get('/logout', (req,res)=>{
   req.logOut();
-  req.send({message: "Successfully logged out."});
-  res.redirect("/users/login")
-})
-
-app.get('/success',  (req,res)=>{
-  res.send({data: {name : req.user.name }})
-})
-app.get('/fail', (req,res)=>{
-  res.send(data)
+  res.send({message: "Successfully logged out."});
 })
 
 
+function auth (req, res, next) {
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.status(403).send({isAuthenticated: false})
+}
 
 
+app.get('/dashboard', auth, (req, res) => {
+  res.status(200).send({isAuthenticated: true})
+});
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
